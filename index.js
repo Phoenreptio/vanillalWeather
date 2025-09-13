128e2cf82eb0db89b99a30e82a240b0b3b06c379
Phoenreptio <serpieri23@gmail.com>
2025-09-13 18:51:03 +0200

    API

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+function updateWeather(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+
+
+cityElement.innerHTML = response.data.city;
+ temperatureElement.innerHTML = Math.round(temperature);
+
+
+}
+
+function searchCity(city) {
+let apiKey ="f6fc8d6e97o4b220033t044a1041dfbf"; 
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
 function handSearchSubmit(event) {
 event.preventDefault();
-let searchInput = document.querySelector('#search-form-input');
-let cityElement = document.querySelector('#weather-app-city');
-cityElement.innerHTML = searchInput.value;
+let searchInput = document.querySelector("#search-form-input");
+
+searchCity(searchInput.value);
 } 
 
-let searchFormElement = document.querySelector('#search-form');
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handSearchSubmit); 
 
+searchCity("Bern"); 
