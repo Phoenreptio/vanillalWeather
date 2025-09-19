782de3af9da1caa3bdb6f75a8b21c35264980b08
Phoenreptio <serpieri23@gmail.com>
2025-09-19 15:00:54 +0200

    removed htlm and injected into JS

--- index.js
+++ index.js
@@ -46,7 +46,36 @@ let searchInput = document.querySelector("#search-form-input");
 searchCity(searchInput.value);
 } 
 
+function displayForecast() {
+let days =["Tue","Wed", "Thu", "Fri", "Sat"];
+let forecastHtml = ""; 
+
+days.forEach(function(day) {
+forecastHtml = 
+forecastHtml + 
+`
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date"> ${day}</div>
+  <div class="weather-forecast-icon">⛅</div>
+  <div class="weather-forecast-temperatures"><strong>15°</strong></div>
+  <div class="weather-forecast-temperature">9°</div>
+</div>
+</div> 
+`; 
+}); 
+}
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml; 
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
 
 searchCity("Bern"); 
+displayForecast();
+
+
+
+
+
+` `
